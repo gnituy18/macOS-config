af3991930a64ee8d1ac8c0f200b3c84093e2aa7f
sourfeethsu <sourfeethsu@gmail.com>
2017-03-05 00:24:20 +0800

    Update install.sh
    Add .tern-project

--- install.sh
+++ install.sh
@@ -10,17 +10,25 @@ brew install bash
 brew install macvim
 brew install git
 brew install cmake
-
 brew install ruby
 brew install python
 brew install python3
+brew install node
+brew install npm
+
 brew cask install google-chrome
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s /usr/local/bin/mvim /usr/local/bin/vim
+if [ -h ~/.vim ] ; then
+	rm -rf ~/.vim
+fi
 ln -s ${DIR}/.vim ~/.vim
 ln -s ${DIR}/.vimrc ~/.vimrc
 ln -s ${DIR}/.bash_profile ~/.bash_profile
 
+git submodule update --init --recursive
 git config --global core.editor vim
+
+${DIR}/.vim/bundle/YouCompleteMe/install.py --clang-completer --tern-completer
