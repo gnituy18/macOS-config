ec1793a73b0d46a038d90c8c35a674d9a751ff4f
sourfeethsu <sourfeethsu@gmail.com>
2017-03-17 09:25:16 +0800

    Add scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s /usr/local/bin/mvim /usr/local/bin/vim
+if [ -h ~/.vim ] ; then
+	rm -rf ~/.vim
+fi
+ln -s ${DIR}/.vim ~/.vim
+ln -s ${DIR}/.vimrc ~/.vimrc
+ln -s ${DIR}/.bash_profile ~/.bash_profile
+ln -s ${DIR}/.tern-project ~/.tern-project
+ln -s ${DIR}/.gitignore ~/.gitignore
+
+git submodule update --init --recursive
+git config --global core.editor vim
+git config --global core.excludesfile ~/.gitignore
+
+${DIR}/.vim/bundle/YouCompleteMe/install.py --clang-completer --tern-completer
